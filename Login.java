53cd51cdcce69ff9d8a8a626a28efdc34f1142f9
Fidel <fidelquispesivana19@gmail.com>
2026-08-24 10:55:47 -0500

    Commint 1: Agrega Login.java

---
+++ Login.java
@@ -0,0 +1,7 @@
+public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Matricula Academica");
+        System.out.println("Inicio de sesion");
+    }
+}
